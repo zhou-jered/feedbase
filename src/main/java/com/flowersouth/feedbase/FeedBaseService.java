4335801b7e326bfe7d2e93dd4e3c552b067e479d
zhou-jered <cyzjylw@126.com>
2020-10-13 17:39:50 +0800

    init

---
+++ src/main/java/com/flowersouth/feedbase/FeedBaseService.java
@@ -0,0 +1,10 @@
+package com.flowersouth.feedbase;
+
+/**
+ * Hello world!
+ */
+public class FeedBaseService {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
